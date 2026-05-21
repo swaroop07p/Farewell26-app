db94113eed76020f9427021c99fc68e74ec2d7c5
Swaroop P <swaroop07p@gmail.com>
2026-05-21 22:46:48 +0530

    temporary poster update 2

--- src/data/seedData.js
+++ src/data/seedData.js
@@ -1,58 +1,67 @@
 export const guestsData = [
-    {
-      usn: "4JN24AI100", // The Admin
-      name: "ADMIN BOSS",
-      invitationImage: "https://res.cloudinary.com/.../admin-invite.jpg",
-      QRImg: "https://res.cloudinary.com/.../admin-qr.jpg", // Pre-generated QR!
-      funnyLine: "I control the gates!",
-      entered: false, // This will be the live toggle
-    },
-    {
-      usn: "4JN24AI058", 
-      name: "SWAROOP P",
-      invitationImage: "https://res.cloudinary.com/dsojrrb9j/image/upload/v1779295172/WhatsApp_Image_2026-05-16_at_7.36.00_PM_t5ppld.jpg",
-      QRImg: "https://res.cloudinary.com/dsojrrb9j/image/upload/v1779295535/WhatsApp_Image_2026-05-20_at_10.13.26_PM_h0aktu.jpg",
-      funnyLine: "Always debugging, rarely sleeping.",
-      entered: false,
-    },
-    {
-      usn: "4JN23AI401", 
-      name: "MANOJ CR",
-      invitationImage: "https://res.cloudinary.com/dsojrrb9j/image/upload/v1779383137/WhatsApp_Image_2026-05-21_at_10.34.06_PM_j9mydr.jpg",
-      QRImg: "https://res.cloudinary.com/dsojrrb9j/image/upload/v1779295535/WhatsApp_Image_2026-05-20_at_10.13.26_PM_h0aktu.jpg",
-      funnyLine: "Manoj CR doesn’t need Google Maps… bro gets lost even in his own thoughts.",
-      entered: false,
-    },
-    {
-      usn: "4JN22AI050", 
-      name: "SHREYAS S",
-      invitationImage: "https://res.cloudinary.com/dsojrrb9j/image/upload/v1779383137/WhatsApp_Image_2026-05-21_at_10.34.06_PM_1_euvv7b.jpg",
-      QRImg: "https://res.cloudinary.com/dsojrrb9j/image/upload/v1779295535/WhatsApp_Image_2026-05-20_at_10.13.26_PM_h0aktu.jpg",
-      funnyLine: "hreyas S can make even a simple story sound like a blockbuster movie.",
-      entered: false,
-    },
-    {
-      usn: "4JN22AI032", 
-      name: "NEERADHI",
-      invitationImage: "",
-      QRImg: "",
-      funnyLine: "Exam time alli Neeradhi confidence nodi teachers ge shock agutte.",
-      entered: false,
-    },
-    {
-      usn: "4JN22AI030", 
-      name: "ANAS",
-      invitationImage: "",
-      QRImg: "",
-      funnyLine: "ANAS phone battery ginta avna excuses jaasti long lasting!",
-      entered: false,
-    },
-    {
-      usn: "A", 
-      name: "A",
-      invitationImage: "https://res.cloudinary.com/.../john-invite.jpg",
-      QRImg: "https://res.cloudinary.com/.../john-qr.jpg",
-      funnyLine: "Always debugging, rarely sleeping.",
-      entered: false,
-    },
-  ];
+  {
+    usn: "4JN24AI100", // The Admin
+    name: "ADMIN BOSS",
+    invitationImage: "https://res.cloudinary.com/.../admin-invite.jpg",
+    QRImg: "https://res.cloudinary.com/.../admin-qr.jpg", // Pre-generated QR!
+    funnyLine: "I control the gates!",
+    entered: false, // This will be the live toggle
+  },
+  {
+    usn: "4JN24AI058",
+    name: "SWAROOP P",
+    invitationImage:
+      "https://res.cloudinary.com/dsojrrb9j/image/upload/v1779295172/WhatsApp_Image_2026-05-16_at_7.36.00_PM_t5ppld.jpg",
+    QRImg:
+      "https://res.cloudinary.com/dsojrrb9j/image/upload/v1779295535/WhatsApp_Image_2026-05-20_at_10.13.26_PM_h0aktu.jpg",
+    funnyLine: "Always debugging, rarely sleeping.",
+    entered: false,
+  },
+  {
+    usn: "4JN23AI401",
+    name: "MANOJ CR",
+    invitationImage:
+      "https://res.cloudinary.com/dsojrrb9j/image/upload/v1779383373/WhatsApp_Image_2026-05-21_at_10.34.06_PM_w8qbko.jpg",
+    QRImg:
+      "https://res.cloudinary.com/dsojrrb9j/image/upload/v1779295535/WhatsApp_Image_2026-05-20_at_10.13.26_PM_h0aktu.jpg",
+    funnyLine:
+      "Manoj CR doesn’t need Google Maps… bro gets lost even in his own thoughts.",
+    entered: false,
+  },
+  {
+    usn: "4JN22AI050",
+    name: "SHREYAS S",
+    invitationImage:
+      "https://res.cloudinary.com/dsojrrb9j/image/upload/v1779383374/WhatsApp_Image_2026-05-21_at_10.34.06_PM_1_hupohe.jpg",
+    QRImg:
+      "https://res.cloudinary.com/dsojrrb9j/image/upload/v1779295535/WhatsApp_Image_2026-05-20_at_10.13.26_PM_h0aktu.jpg",
+    funnyLine:
+      "hreyas S can make even a simple story sound like a blockbuster movie.",
+    entered: false,
+  },
+  {
+    usn: "4JN22AI032",
+    name: "NEERADHI",
+    invitationImage: "",
+    QRImg: "",
+    funnyLine:
+      "Exam time alli Neeradhi confidence nodi teachers ge shock agutte.",
+    entered: false,
+  },
+  {
+    usn: "4JN22AI030",
+    name: "ANAS",
+    invitationImage: "",
+    QRImg: "",
+    funnyLine: "ANAS phone battery ginta avna excuses jaasti long lasting!",
+    entered: false,
+  },
+  {
+    usn: "A",
+    name: "A",
+    invitationImage: "https://res.cloudinary.com/.../john-invite.jpg",
+    QRImg: "https://res.cloudinary.com/.../john-qr.jpg",
+    funnyLine: "Always debugging, rarely sleeping.",
+    entered: false,
+  },
+];
